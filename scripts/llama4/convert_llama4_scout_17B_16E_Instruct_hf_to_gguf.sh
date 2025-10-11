eea3ae691e4a4c9139f997b846cde8fffaaccda6
Neel Maulik Patel <nmp83@sapphire.ece.cornell.edu>
2025-10-11 17:45:14 +0000

    change path

--- scripts/llama4/convert_llama4_scout_17B_16E_Instruct_hf_to_gguf.sh
+++ scripts/llama4/convert_llama4_scout_17B_16E_Instruct_hf_to_gguf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-CONVERTER=./third-party/llama.cpp/convert_hf_to_gguf.py 
-HF_SNAPSHOT_DIR=~/.cache/huggingface/models--meta-llama--Llama-4-Scout-17B-16E-Instruct/snapshots/92f3b1597a195b523d8d9e5700e57e4fbb8f20d3/
+CONVERTER_DIR=./third-party/llama.cpp/
+CONVERTER=$CONVERTER_DIR//convert_hf_to_gguf.py 
+HF_SNAPSHOT_DIR=~/.cache/huggingface/hub/models--meta-llama--Llama-4-Scout-17B-16E-Instruct/snapshots/92f3b1597a195b523d8d9e5700e57e4fbb8f20d3/
 GGUF_FILE=llama-4-scout-17b-16e-instruct.gguf
 OUT_DTYPE=bf16
 
-$CONVERTER $HF_SNAPSHOT_DIR --outfile $GGUF_FILE --outtype $OUT_DTYPE
+python $CONVERTER $HF_SNAPSHOT_DIR --outfile $GGUF_FILE --outtype $OUT_DTYPE
